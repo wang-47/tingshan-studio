87cf15cbbc4e4bb31f1d2a735756553cc981f441
wang-47 <1476005843@qq.com>
2026-08-10 16:42:41 +0800

    fix vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  basePath: "/tingshan-studio",
-  assetPrefix: "/tingshan-studio/",
-
   images: {
     unoptimized: true,
   },
